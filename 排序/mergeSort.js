ebf8639d43112397f55c216f1a701eda724fd51f
qzuser <1522502267@qq.com>
2019-08-05 07:06:03 +0800

    代码完善

---
+++ 排序/mergeSort.js
@@ -0,0 +1,44 @@
+function MERGESORT(arr) {
+  if (arr == null && arr.length < 2) {
+    return;
+  }
+  mergeSort(arr, 0, arr.length - 1);
+}
+function mergeSort(arr, l, r) {
+  if (l == r) {
+    return;
+  }
+  var mid = Math.floor((l + r) / 2);
+  mergeSort(arr, l, mid);
+  mergeSort(arr, mid + 1, r);
+  merge(arr, l, mid, r);
+}
+function merge(arr, l, m, r) {
+  var help = new Array(r - l + 1);
+  var p1 = l,
+    p2 = m + 1,
+    i = 0;
+  while (p1 <= m && p2 <= r) {
+    if (arr[p2] > arr[p1]) {
+      sum += arr[p1] * (r - p2 + 1);
+    }
+    help[i++] = arr[p2] > arr[p1] ? arr[p1++] : arr[p2++];
+  }
+  while (p1 <= m) {
+    help[i++] = arr[p1++];
+  }
+  while (p2 <= r) {
+    help[i++] = arr[p2++];
+  }
+  for (let i = 0; i < help.length; i++) {
+    arr[l + i] = help[i];
+  }
+}
+function main() {
+  var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+  MERGESORT(arr);
+  console.log(arr);
+  console.log(sum);
+}
+var sum = 0;
+main();
