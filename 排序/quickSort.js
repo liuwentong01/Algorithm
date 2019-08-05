90b522b2edea71ca8023951d91dc6670e857589a
qzuser <1522502267@qq.com>
2019-08-06 07:11:38 +0800

    。

--- 排序/quickSort.js
+++ 排序/quickSort.js
@@ -1,23 +1,43 @@
-function quickSort0(arr, low, high){
-  var i = low, j = high;
-  var baseValue = arr[low];
-  if(i >= j){
-    return arr;
+function QUICK(arr){
+  if(arr == null || arr.length < 2){
+    return;
   }
-  while(i != j){
-    while(i<j && arr[j] >= baseValue) j--;
-    while(i<j && arr[i] <= baseValue) i++;
-    if(i < j){
-      var temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+  quickSort(arr, 0, arr.length-1)
+}
+
+function quickSort(arr, L, R){
+  if(L < R){
+    var cur = dutchFlag(arr, L, R);
+    quickSort(arr, L, cur[0]-1);
+    quickSort(arr, cur[1]+1, R);
+  }
+  
+}
+
+function dutchFlag(arr, L, R) {
+  var less = L - 1, more = R;
+  while (L < more) {
+    if (arr[L] < arr[R]) {
+      swap(arr, ++less, L++);
+    } else if (arr[L] > arr[R]) {
+      swap(arr, --more, L);
+    } else {
+      L++;
     }
   }
-  arr[low] = arr[i];
-  arr[i] = baseValue;
-  quickSort0(arr, low, i-1);
-  quickSort0(arr,i+1, high);
-  return arr;
+  swap(arr, L, R)
+  return [less + 1, more];
 }
-var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
-console.log(quickSort0(arr, 0, 14));
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function main(){
+  var arr = [2, 15, 26, 27, 44, 46, 48, 50, 3, 4, 5, 36, 38, 47, 19];
+  QUICK(arr);
+  console.log(1);
+  console.log(arr);
+}
+main()
